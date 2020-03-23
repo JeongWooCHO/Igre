dc5e0d7b34bfe7a0ae63e93afa60431273d815f9
CHO <CHO@DESKTOP-RONJRE2>
2020-03-23 17:23:42 +0900

    닫아보자 ..분기점을

---
+++ Test/src/com/kh/test/close.java
@@ -0,0 +1,7 @@
+package com.kh.test;
+
+public class close {
+public static void main(String[] args) {
+	System.out.println("닫아버리귀");
+}
+}
--- Test/src/com/kh/test/testGit.java
+++ Test/src/com/kh/test/testGit.java
@@ -8,6 +8,7 @@ System.out.println("테스트 깃.");
 System.out.println("테스트 깃2.");
 System.out.println("정우 브랜치에서 입력합니다.");
 System.out.println("지준형나빠요~");
+
 	}
 
 }
