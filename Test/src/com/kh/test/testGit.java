f83caf1b0fc6fdc7514c3c1ba296be9a62781605
kosanguk92 <59955312+kosanguk92@users.noreply.github.com>
2020-03-23 16:33:22 +0900

    2020/03/23
    1.부탁할게

--- Test/src/com/kh/test/testGit.java
+++ Test/src/com/kh/test/testGit.java
@@ -5,10 +5,7 @@ public class testGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("테스트 깃.");
-System.out.println("테스트 깃2.");
-System.out.println("테스트 깃3.");
-System.out.println("수웩");
-System.out.println("고수웩");
+
 	}
 
 }
